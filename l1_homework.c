570577603fc0434fa99c8d5aae4b6f4b4a7905ff
VictoriaMakaryan <138680813+VictoriaMakaryan@users.noreply.github.com>
2023-07-05 16:24:43 +0400

    Add files via upload

---
+++ l1_homework.c
@@ -0,0 +1,64 @@
+
+
+// problem 1
+/*#include <stdio.h>
+
+int main()
+{
+	int var = 0;
+	if (var == 0)
+		printf("var == 0 \n"), printf("hier kommt die sonne \n");
+} 
+
+//problem 2
+#include <stdio.h>
+
+int main()
+{
+	int num = 0;
+	printf("Please input a number: \n");
+	scanf("%i", &num);
+
+	if (num % 2 == 0)
+	{
+		printf("The number is even! \n");
+	} else 
+	{
+		printf("The number is odd! \n");
+	}
+}*/
+
+//problem 3
+ 
+/*
+#include <stdio.h>
+
+int main()
+{
+	int num;
+	int count = 0;
+	int i;
+	printf("Please input a number \n");
+	scanf("%i", &num);
+
+	if (num == 0 || num == 1)
+		count = 1;
+
+	for (i = 2; i <= (num / 2); i++)
+	{
+		if (num % i == 0)
+		{
+			count = 1;
+			break;
+		}
+	}
+	
+	if (count == 1)
+	{
+		printf("Your number is not prime! \n");
+	} else
+	{
+		printf("Your number is prime! \n");
+	}
+}
+*/
